fc8a8eee5fcab13c3f0e32ad6d24a04d417c0bf6
AlvesLinux <thicoalves@hotmail.com>
2021-06-18 16:07:53 -0300

    novo commit

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
 <head>
     <title></title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <script type="text/javascript">
-        $(function(){
-            let v = parseFloat('1000.50');
-
-            $('div').html(v.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}) );
-        });
-    </script>
+    <script src="assets/js/scripts.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/style.css">    
     
 </head>
 <body>
-        
-    <div></div>    
-
+    <div class="img">
+        <img class="img img-responsive" src="assets/image/1.jpg" alt="Exemplo de site" srcset=""> 
+    </div>       
 </body>
 </html>
